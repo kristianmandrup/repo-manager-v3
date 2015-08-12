352e3bab519cf8c99e482cdae03c11b29a8f7558
Kristian Mandrup <kmandrup@gmail.com>
2015-08-12 21:03:00 +0200

    simplified data binding ;)

--- apps/index/page/index.marko.js
+++ apps/index/page/index.marko.js
@@ -3,18 +3,12 @@ function create(__helpers) {
       empty = __helpers.e,
       notEmpty = __helpers.ne,
       __renderer = __helpers.r,
-      _______global_components_hello_you_renderer_js = __renderer(require("../../_global/components/hello-you/renderer")),
-      __tag = __helpers.t,
       _______global_components_icon_list_item_renderer_js = __renderer(require("../../_global/components/icon-list-item/renderer")),
+      __tag = __helpers.t,
       _________node_modules_browser_refresh_taglib_refresh_tag_js = __renderer(require("browser-refresh-taglib/refresh-tag"));
 
   return function render(data, out) {
-    out.w('<!DOCTYPE html> <html><head><link rel="stylesheet" type="text/css" href="semantic.min.css"><script src="semantic/dist/semantic.min.js">   </script><title>try2 Title</title><lasso-head></lasso-head></head><body><h1>try2</h1>');
-    __tag(out,
-      _______global_components_hello_you_renderer_js,
-      {
-        "label": "James"
-      });
+    out.w('<!DOCTYPE html> <html><head><link rel="stylesheet" type="text/css" href="semantic.min.css"><script src="semantic/dist/semantic.min.js">   </script><title>try2 Title</title><lasso-head></lasso-head></head><body><h1>Repo Manager</h1>');
     __tag(out,
       _______global_components_icon_list_item_renderer_js,
       {
