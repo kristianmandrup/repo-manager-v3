a0c08d72f02eb56bb62ff72cad79b559d814fb68
Kristian Mandrup <kmandrup@gmail.com>
2015-08-14 07:01:26 +0200

    extract component listers to marooka :)

--- apps/_global/index.js
+++ apps/_global/index.js
@@ -1,12 +1,1 @@
-var path = require('path');
-var markoa = require('marooka')
-var components = markoa.components;
-var componentsPath = path.join(__dirname, 'components')
-
-components.categorizer(componentsPath, function(finder) {
-  var comps = finder();
-  var compsListPath = path.join(componentsPath, 'components-map.json');
-  components.writer.toJsonFile(compsListPath, comps, function() {
-    // console.log('DONE');
-  });
-});
+require('marooka').components.categorize(__dirname);
