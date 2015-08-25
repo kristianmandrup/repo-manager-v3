e6d3d3950314bf00aef169ee5a81bc81e16dba20
Kristian Mandrup <kmandrup@gmail.com>
2015-08-25 12:37:24 +0200

    debugging taglibs

--- apps/index/page/index.marko.js
+++ apps/index/page/index.marko.js
@@ -4,8 +4,9 @@ function create(__helpers) {
       notEmpty = __helpers.ne,
       escapeXml = __helpers.x,
       __renderer = __helpers.r,
-      _________node_modules_browser_refresh_taglib_refresh_tag_js = __renderer(require("browser-refresh-taglib/refresh-tag")),
-      __tag = __helpers.t;
+      _______global_components_icon_list_renderer_js = __renderer(require("../../_global/components/icon-list/renderer")),
+      __tag = __helpers.t,
+      _________node_modules_browser_refresh_taglib_refresh_tag_js = __renderer(require("browser-refresh-taglib/refresh-tag"));
 
   return function render(data, out) {
     out.w('<!DOCTYPE html> <html><head><link rel="stylesheet" type="text/css" href="semantic.min.css"><script src="https://code.jquery.com/jquery-2.1.4.min.js">   </script><script src="semantic.min.js">   </script><title>' +
@@ -14,7 +15,19 @@ function create(__helpers) {
       escapeXml(data.header) +
       ' </h1><h2>' +
       escapeXml(out.global.appName) +
-      '</h2><icon-list items="out.global.lists.users"></icon-list><icon-list items="out.global.lists.repos"></icon-list><lasso-body></lasso-body>');
+      '</h2>');
+    __tag(out,
+      _______global_components_icon_list_renderer_js,
+      {
+        "list": data.lists.users
+      });
+    __tag(out,
+      _______global_components_icon_list_renderer_js,
+      {
+        "list": out.global.lists.repos
+      });
+
+    out.w('<lasso-body></lasso-body>');
     __tag(out,
       _________node_modules_browser_refresh_taglib_refresh_tag_js,
       {
--- apps/index/state/index.js
+++ apps/index/state/index.js
@@ -2,6 +2,20 @@ module.exports = {
   page: {
     title: 'Repo manager',
     header: 'Repo manager',
-    content: 'Hello'    
+    content: 'Hello',
+    lists: {
+      users: [{
+        ui: 'large orange',
+        icon: 'user',
+        label: 'kmandrup',
+        desc: 'I DREAM in CODE'
+      },
+      {
+        ui: 'large blue',
+        icon: 'user',
+        label: 'isaura',
+        desc: 'I LOVE web DESIGN'
+      }]
+    }
   }
 }
