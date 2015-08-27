f1649d2dbbe422e5bd0150121a663b5d82bfb4a8
Kristian Mandrup <kmandrup@gmail.com>
2015-08-27 17:00:16 +0100

    checkbox type demo

--- apps/index/page/index.marko.js
+++ apps/index/page/index.marko.js
@@ -8,10 +8,7 @@ function create(__helpers) {
       __renderer = __helpers.r,
       ____________semantic_tags_semantic_components_form_ui_form_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/form/ui-form/renderer")),
       __tag = __helpers.t,
-      ____________semantic_tags_semantic_components_form_input_check_boxes_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/form/input/check-boxes/renderer")),
-      ____________semantic_tags_semantic_components_form_field_ui_fields_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/form/field/ui-fields/renderer")),
-      ____________semantic_tags_semantic_components_form_field_ui_field_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/form/field/ui-field/renderer")),
-      ____________semantic_tags_semantic_components_form_input_line_input_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/form/input/line-input/renderer")),
+      ____________semantic_tags_semantic_components_form_input_check_box_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/form/input/check-box/renderer")),
       _________node_modules_browser_refresh_taglib_refresh_tag_js = __renderer(require("browser-refresh-taglib/refresh-tag"));
 
   return function render(data, out) {
@@ -21,71 +18,33 @@ function create(__helpers) {
       escapeXmlAttr(data.name) +
       '.browser.json"></lasso-page><!DOCTYPE html> <html><head><link rel="stylesheet" type="text/css" href="semantic.min.css"><script src="https://code.jquery.com/jquery-2.1.4.min.js">   </script><script src="semantic.min.js">   </script><title>' +
       escapeXml(data.title) +
-      '</title><lasso-head></lasso-head></head><body><h1>' +
-      escapeXml(data.header) +
-      ' </h1><h2>' +
-      escapeXml(out.global.appName) +
-      '</h2><h3>Basic field</h3>');
+      '</title><lasso-head></lasso-head></head><body><h3>Sliders</h3>');
     __tag(out,
       ____________semantic_tags_semantic_components_form_ui_form_renderer_js,
       {},
       function(out) {
         __tag(out,
-          ____________semantic_tags_semantic_components_form_input_check_boxes_renderer_js,
+          ____________semantic_tags_semantic_components_form_input_check_box_renderer_js,
           {
-            "list": data.languages,
-            "label": "Languages"
+            "name": "male",
+            "checked": true,
+            "field": "Male",
+            "type": "slider"
           });
-      });
-
-    out.w('<h3>User form</h3>');
-    __tag(out,
-      ____________semantic_tags_semantic_components_form_ui_form_renderer_js,
-      {},
-      function(out) {
         __tag(out,
-          ____________semantic_tags_semantic_components_form_field_ui_fields_renderer_js,
+          ____________semantic_tags_semantic_components_form_input_check_box_renderer_js,
           {
-            "list": data.userForm
+            "name": "male",
+            "checked": true,
+            "field": "Male"
           });
-      });
-
-    out.w('<h3>Custom form</h3>');
-    __tag(out,
-      ____________semantic_tags_semantic_components_form_ui_form_renderer_js,
-      {},
-      function(out) {
         __tag(out,
-          ____________semantic_tags_semantic_components_form_field_ui_fields_renderer_js,
-          {},
-          function(out) {
-            __tag(out,
-              ____________semantic_tags_semantic_components_form_field_ui_field_renderer_js,
-              {
-                "ui": "small blue",
-                "label": "Username"
-              },
-              function(out) {
-                __tag(out,
-                  ____________semantic_tags_semantic_components_form_input_line_input_renderer_js,
-                  {
-                    "name": "username"
-                  });
-              });
-            __tag(out,
-              ____________semantic_tags_semantic_components_form_field_ui_field_renderer_js,
-              {
-                "ui": "small green",
-                "label": "Secret"
-              },
-              function(out) {
-                __tag(out,
-                  ____________semantic_tags_semantic_components_form_input_line_input_renderer_js,
-                  {
-                    "name": "secret",
-                    "type": "password"
-                  });
-              });
+          ____________semantic_tags_semantic_components_form_input_check_box_renderer_js,
+          {
+            "name": "male",
+            "checked": true,
+            "field": "Male",
+            "type": "toggle"
           });
       });
 
