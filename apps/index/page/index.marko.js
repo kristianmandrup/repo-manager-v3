9c95b48b11bddbde50ccb00540f5ada1d9c0f4ae
Kristian Mandrup <kmandrup@gmail.com>
2015-09-06 18:36:41 +0200

    main layout wip

--- apps/index/page/index.marko.js
+++ apps/index/page/index.marko.js
@@ -6,45 +6,36 @@ function create(__helpers) {
       escapeXmlAttr = __helpers.xa,
       escapeXml = __helpers.x,
       __renderer = __helpers.r,
-      ____________semantic_tags_semantic_components_form_ui_form_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/form/ui-form/renderer")),
+      ____________semantic_tags_semantic_components_menu_top_menu_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/menu/top-menu/renderer")),
       __tag = __helpers.t,
-      ____________semantic_tags_semantic_components_form_input_check_box_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/form/input/check-box/renderer")),
+      ____________semantic_tags_semantic_components_sidebar_ui_sidebar_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/sidebar/ui-sidebar/renderer")),
+      ____________semantic_tags_semantic_components_sidebar_sidebar_menus_renderer_js = __renderer(require("../../../../semantic-tags/semantic/components/sidebar/sidebar-menus/renderer")),
       _________node_modules_browser_refresh_taglib_refresh_tag_js = __renderer(require("browser-refresh-taglib/refresh-tag"));
 
   return function render(data, out) {
     out.w('<lasso-page' +
       attr("name", data.name) +
-      ' package-path="./' +
+      ' package-path="./dependencies/' +
       escapeXmlAttr(data.name) +
       '.browser.json"></lasso-page><!DOCTYPE html> <html><head><link rel="stylesheet" type="text/css" href="semantic.min.css"><script src="https://code.jquery.com/jquery-2.1.4.min.js">   </script><script src="semantic.min.js">   </script><title>' +
       escapeXml(data.title) +
-      '</title><lasso-head></lasso-head></head><body><h3>Sliders</h3>');
+      '</title><lasso-head></lasso-head></head><body>');
     __tag(out,
-      ____________semantic_tags_semantic_components_form_ui_form_renderer_js,
-      {},
+      ____________semantic_tags_semantic_components_menu_top_menu_renderer_js,
+      {
+        "ui": "main fixed inverted borderless",
+        "menu": data.top.menu
+      });
+    __tag(out,
+      ____________semantic_tags_semantic_components_sidebar_ui_sidebar_renderer_js,
+      {
+        "ui": "left vertical inverted labeled icon"
+      },
       function(out) {
         __tag(out,
-          ____________semantic_tags_semantic_components_form_input_check_box_renderer_js,
+          ____________semantic_tags_semantic_components_sidebar_sidebar_menus_renderer_js,
           {
-            "name": "male",
-            "checked": true,
-            "field": "Male",
-            "type": "slider"
-          });
-        __tag(out,
-          ____________semantic_tags_semantic_components_form_input_check_box_renderer_js,
-          {
-            "name": "male",
-            "checked": true,
-            "field": "Male"
-          });
-        __tag(out,
-          ____________semantic_tags_semantic_components_form_input_check_box_renderer_js,
-          {
-            "name": "male",
-            "checked": true,
-            "field": "Male",
-            "type": "toggle"
+            "menus": data.sidebar.menus
           });
       });
 
