1848a82ede36f46cfc63e320e5fa3b2bf6293ddb
Kristian Mandrup <kmandrup@gmail.com>
2015-09-13 16:38:55 +0200

    app index

--- apps/project/index.js
+++ apps/project/index.js
@@ -1,5 +1,18 @@
+/*jslint node: true */
+'use strict';
+
 module.exports = {
   meta: require('./meta'),
   data: require('./data'),
-  apps: require('./apps')
-}
+  apps: require('./apps'),
+  methods: {
+    post: function*(next) {
+      console.log('POST');
+      yield next;
+    },
+    update: function*(next) {
+      console.log('UPDATE');
+      yield next;
+    }
+  }
+};
